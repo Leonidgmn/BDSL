113e25b381965f9d4eb763334b7685499ee837e3
Mykola <mykola.lwiv@gmail.com>
2022-08-05 21:28:28 +0300

    add form v1.beta, edited navigation menu

---
+++ src/components/UI/modal/MyModal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './myModal.module.css'
+const MyModal = ({children, visible, setVisible}) => {
+    const classVisible=[classes.myModal];
+    if (visible){
+        classVisible.push(classes.active)
+    }
+    return (
+        <div className={classVisible.join(' ')} onClick={()=>setVisible(false)}>
+            <div className={classes.modalContent}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default MyModal;
