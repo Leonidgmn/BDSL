7ec53977214d676063ecf5d26029720b0bfb4f3b
Leonid <102416306+Leonidgmn@users.noreply.github.com>
2022-08-05 21:07:58 +0300

    add google map, contact info, link to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
                 <div className="contacts">
 
                     <div className="contacts-socials">
-                        <a href="#"><i className="bi bi-facebook"></i></a>
-                        <a href="#"><i className="bi bi-instagram"></i></a>
-                        <a href="#"><i className="bi bi-telegram"></i></a>
+                        <a href="https://www.facebook.com/BDSLviv" target="_blank"><i className="bi bi-facebook"></i></a>
+                        <a href="https://www.instagram.com/bdslviv/" target="_blank"><i className="bi bi-instagram"></i></a>
+                        <a href="https://t.me/bdslviv/" target="_blank"><i className="bi bi-telegram"></i></a>
                     </div>
 
 
