432db6408057f6cbf8d2408428114a779abdfc56
Mariana205 <kozopasmaryna@gmail.com>
2022-08-27 14:06:17 +0300

    WB-11/ added team/data

---
+++ src/data/teamData.js
@@ -0,0 +1,33 @@
+export const teamData = [{
+        id: 1,
+        name: 'Катерина Патрон',
+        dance: 'Kizomba',
+        photo: 'https://i.postimg.cc/3RkwyX6R/Ihor-Olesia1.jpg',
+        description: 'Танцювальний талант і володіння різними стилями танцю. Музичний слух. Творче мислення і бачення',
+        quote: 'якась розумна цитата викладача'
+    },
+    {
+        id: 2,
+        name: 'Олена Патрон',
+        dance: 'Salsa',
+        photo: 'https://i.postimg.cc/Z5DtWGh8/Halia-Taras.jpg',
+        description: 'Музичний слух. Навички викладання(якщо працює з дітьми, то ще й педагогічні знання). Творче мислення і бачення',
+        quote: 'цитата викладача'
+    },
+    {
+        id: 3,
+        name: 'Віктор Ільчишин',
+        dance: 'Zumba',
+        photo: 'https://i.postimg.cc/wM9bf2px/Misha-Zenia.jpg',
+        description: 'Танцювальний талант і володіння різними стилями танцю.',
+        quote: 'якась розумна цитата'
+    },
+    {
+        id: 4,
+        name: 'Олена Писар',
+        dance: 'Bachata',
+        photo: 'https://i.postimg.cc/xTxF5LFY/Myko-Zenia.jpg',
+        description: 'Танцювальний талант і володіння різними стилями танцю. Музичний слух. Навички викладання(якщо працює з дітьми, то ще й педагогічні знання). Творче мислення і бачення',
+        quote: 'розумна цитата'
+    }
+];
