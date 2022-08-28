26f206fad933ad5a3caf38cb203b2f26135fe3f5
Alexman604 <goa-man-604@ukr.net>
2022-08-28 15:59:05 +0300

    active item menu and google_id added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import MainPage from "./components/mainPage";
 import Header from "./components/header";
 import Footer from "./components/footer";
@@ -9,9 +9,36 @@ import News from "./components/news";
 import Contacts from "./components/contacts";
 import Respond from "./components/respond";
 import Dance from "./components/dance";
+import jwt_decode from "jwt-decode"
+
 
 function App() {
+
+    const [user, setUser] = useState({})
+
+    function handleCallbackResponse(response) {
+        console.log("Encoded JDT ID token: " + response.credential)
+        let userObject = jwt_decode(response.credential)
+        console.log(userObject)
+        setUser(userObject)
+       // document.getElementById("signInDiv").hidden = true;
+
+    }
+    
+    useEffect(()=> {
+        /* global google*/
+        google.accounts.id.initialize ({
+            client_id: "649976560125-i5k6ng1hi6kkv2i2e9lrchqa4979mvia.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        })
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            {theme:"outline", size: "large"}
+
+        )
+    },[])
     const [modal, setModal] = useState(false);
+  
 
     return (
         <div className="App">
