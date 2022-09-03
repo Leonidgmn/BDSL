af8e8f53f656c431d3c10d5de58264d38225b3ac
Mykola <mykola.lwiv@gmail.com>
2022-09-04 00:36:08 +0300

    add bd
    add bd to team. posibiliti to add and delete new team

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
-import { teamData } from '../data/teamData.js';
+import React, {useContext, useState} from 'react';
+import {Context} from "../index";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import MyButton from "./UI/button/MyButton";
+import firebase from "firebase/compat/app";
+import MyModal from "./UI/modal/MyModal";
+import MyInput from "./UI/input/MyInput";
 
 const Team = () => {
+    const [modal, setModal] = useState(false);
+    const {firestore}=useContext(Context)
+    const [teams, setTeams]=useState({
+                name:'',
+                photo:'',
+                dance:'',
+                description:'',
+                quote:''
+    })
+    const [team, loading]=useCollectionData(
+        firestore.collection('teamData')
+    )
+    if (loading){
+        return
+    }
+    const newTeam= async (e) =>{
+        e.preventDefault()
+       await firestore.collection('teamData').add({
+            id: firebase.firestore.FieldValue.serverTimestamp(),
+            name:teams.name,
+            photo:teams.photo,
+            dance:teams.dance,
+            description:teams.description,
+            quote:teams.quote
+})
+    setTeams({
+        name:'',
+        photo:'',
+        dance:'',
+        description:'',
+        quote:''
+    })
+    }
+    const deleteTeam=(id)=>{
+       const deleteItem = firestore.collection('teamData').where('id', '==', id);
+        deleteItem.get().then(function (querySnapshot){
+            querySnapshot.forEach(function (doc){
+                doc.ref.delete()
+            })
+        })
+
+    }
     return (
         <section id="team">
             <div className="content">
                 <h2>Наша команда</h2>
                 <div className="team-list">
-                    {teamData.map((team) => (
+                    {team.map((team) => (
                         <div className='team-item left' key={team.id}>
                             <div><img src={team.photo} alt="" /></div>
                             <div className="team-item-description">
@@ -15,10 +62,48 @@ const Team = () => {
                                 <h4>{team.dance}</h4>
                                 <p>{team.description}</p>
                                 <span>{team.quote}</span>
+                                <MyButton onClick={()=>deleteTeam(team.id)}>видалити викладачів</MyButton>
                             </div>
                         </div>
                     ))}
                 </div>
+                <MyModal visible={modal} setVisible={setModal}>
+                    <form onSubmit={newTeam}>
+                        <MyInput type="text"
+                                 placeholder="Імена викладачів"
+                                 maxLength='40'
+                                 value={teams.name}
+                                 onChange={e=>setTeams({...teams, name:e.target.value})}
+                                 required/>
+                        <MyInput type="text"
+                                 placeholder="який танець викладають"
+                                 maxLength='20'
+                                 value={teams.dance}
+                                 onChange={e=>setTeams({...teams, dance:e.target.value})}
+                                 required/>
+
+                        <MyInput style={{paddingBottom:'1.5rem'}}
+                                 placeholder="опис танцю"
+                                 type="text"
+                                 value={teams.description}
+                                 onChange={e=>setTeams({...teams, description:e.target.value})}
+                                 required/>
+                        <MyInput style={{marginBottom:'1rem'}}
+                                 placeholder="URL зображення викладачів"
+                                 type="text"
+                                 onChange={e=>setTeams({...teams, photo:e.target.value})}
+                                 value={teams.photo}/>
+                        <MyInput style={{marginBottom:'1rem'}}
+                                 placeholder="коротка цитата"
+                                 type="text"
+                                 onChange={e=>setTeams({...teams, quote:e.target.value})}
+                                 value={teams.quote}/>
+
+
+                        <MyButton type="submit" > Відправити</MyButton>
+                    </form>
+                </MyModal>
+                <MyButton onClick={()=>setModal(true)}>добавити викладачів</MyButton>
             </div>
         </section>
     );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
 
+firebase.initializeApp({
+    apiKey: "AIzaSyB-wi7xZXp3Pwm9AvIkU8ZY8EovUWuqxXk",
+    authDomain: "bdsl-ecab9.firebaseapp.com",
+    projectId: "bdsl-ecab9",
+    storageBucket: "bdsl-ecab9.appspot.com",
+    messagingSenderId: "607276073329",
+    appId: "1:607276073329:web:3d48d0bf9d9d10d880b6d0",
+    measurementId: "G-2Q9QWY77JL"
+});
+const firestore=firebase.firestore();
+export const Context=createContext(null);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <Context.Provider value={
+          {firebase, firestore}
+      }>
       <BrowserRouter>
+
           <App />
+
       </BrowserRouter>
+      </Context.Provider>
   </React.StrictMode>
 );
 
